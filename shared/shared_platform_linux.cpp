3faaa9b8e4d9fda2ab778a2203c8a714977f9a8e
Decstar77 <34284628+Decstar77@users.noreply.github.com>
2023-08-27 10:08:11 +0200

    ubuntu fixes

---
+++ shared/shared_platform_linux.cpp
@@ -0,0 +1,17 @@
+#ifdef __linux__
+
+#include <stdio.h>
+void PlatformAssert(const char * msg, const char * file, int line) {
+    printf("Assertion failed: %s\nFile: %s\nLine: %d\n", msg, file, line);
+    exit(1);
+}
+
+const char * PlatformFileDialogOpen(const char * path, const char * filter) {
+    return nullptr;
+}
+
+const char * PlatformFileDialogSave(const char * path, const char * filter){
+    return nullptr;
+}
+
+#endif
